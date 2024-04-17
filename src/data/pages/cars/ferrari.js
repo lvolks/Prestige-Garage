4447fbd9d5dad6d3c13274cc1c405f5171b9780b
lvolks <professionalvolks@gmail.com>
2024-04-17 01:39:25 -0300

    Contents, Footer index and css, page cars

---
+++ src/data/pages/cars/ferrari.js
@@ -0,0 +1,12 @@
+import Header from '../../components/Footer/index.js';
+import Footer from '../../components/Header/index.js';
+
+export default function Ferrari() {
+    return (
+        <div>
+            <Header />
+
+            <Footer />
+        </div>
+    )
+}
---
+++ src/data/pages/cars/porsche.js
@@ -0,0 +1,12 @@
+import Header from '../../components/Footer/index.js';
+import Footer from '../../components/Header/index.js';
+
+export default function Porsche() {
+    return (
+        <div>
+            <Header />
+
+            <Footer />
+        </div>
+    )
+}
